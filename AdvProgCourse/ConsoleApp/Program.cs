d81624673f7e049a794d4a35baf69538cab8432a
avillalon-dev <avillalon.dev@gmail.com>
2022-10-05 13:10:46 -0400

    Create console application
    Add salary functions

---
+++ AdvProgCourse/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Debug.WriteLine("Line");
+            var humanResources = new HumanResources(new List<SalaryFunction>() { SalaryFunctions.WorkerSalary});
+            humanResources.AddWorker("Juan", new DateTime(1995, 12, 24), "", 1, Departments.Automation);
+            humanResources.Activate();
+        }
+    }
+}
---
+++ AdvProgCourse/Model/SalaryFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public static class SalaryFunctions
+    {
+        public static double WorkerSalary(Person person)
+        {
+            if (person is Worker worker)
+            {
+                throw new NotImplementedException();
+            }
+            else
+                throw new ArgumentException("Invalid person type");
+        }
+    }
+}
