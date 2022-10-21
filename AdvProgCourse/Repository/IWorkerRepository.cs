3fc676a60cfcbbf94be51ec961a0f81e774cbfb7
avillalon <avillalon@emsifarma.cu>
2022-10-21 10:05:01 -0400

    Add worker repository

---
+++ AdvProgCourse/Repository/IWorkerRepository.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    /// <summary>
+    /// Define las funcionalidades para la administración de trabajadores.
+    /// </summary>
+    public interface IWorkerRepository : IRepository
+    {
+        /// <summary>
+        /// Crea un trabajador en el soporte de datos.
+        /// </summary>
+        /// <param name="name">
+        /// Nombre del trabajador
+        /// </param>
+        /// <param name="department">
+        /// Departamento al que pertenece el trabajador.
+        /// </param>
+        /// <returns>
+        /// Un objeto <see cref="Worker"/> con la información del trabajador
+        /// </returns>
+        Worker CreateWorker(string name, Departments department);
+    }
+}
---
+++ AdvProgCourse/Repository/WorkerRepository.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    /// <summary>
+    /// Implementa métodos para la administración de los trabajadores
+    /// en la base de datos.
+    /// </summary>
+    public partial class DBRepository : IWorkerRepository
+    {
+        /// <summary>
+        /// Crea un trabajador en la base de datos.
+        /// </summary>
+        /// <param name="name">
+        /// Nombre del trabajador
+        /// </param>
+        /// <param name="department">
+        /// Departamento al que pertenece el trabajador.
+        /// </param>
+        /// <returns>
+        /// Un objeto <see cref="Worker"/> con la información del trabajador
+        /// </returns>
+        public Worker CreateWorker(string name, Departments department)
+        {
+            var worker = new Worker(name, department);
+            Add(worker);
+            return worker;
+        }
+    }
+}
